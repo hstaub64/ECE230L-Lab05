f01934381f15a53078512aac0a7a738eb9fb8272
Haylee Staub <142932978+hstaub64@users.noreply.github.com>
2025-09-26 10:40:01 -0600

    Add files via upload

--- top.v
+++ top.v
@@ -1,1 +1,26 @@
-// Implement top level module
+module top(
+    input [6:0] sw,
+    output [1:0] led
+);
+
+wire inputA;
+
+    circuit_a maxterm_inst(
+        .A(sw[0]),
+        .B(sw[1]),
+        .C(sw[2]),
+        .D(sw[3]),
+        .Y(inputA)
+    );
+    
+    circuit_b minterm_inst(
+        .A(inputA),
+        .B(sw[4]),
+        .C(sw[5]),
+        .D(sw[6]),
+        .Y(led[1])
+    );
+    
+assign led[0] = inputA;
+
+endmodule
